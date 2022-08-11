9e44370f58274e3618f1c39f306617ddb2e85539
Arrow-Li <jeep3333@gmail.com>
2022-08-11 11:14:17 +0800

    Update api.

--- convert.go
+++ convert.go
@@ -30,20 +30,38 @@ func request(api, key, model string) (map[string]interface{}, error) {
 func convert(model, amount string) string {
 	var rate float64
 
+	retry:=false
 	data, err := request(API_URL_1, API_KEY_1, model)
-	if err != nil {
+	if err ==nil{
+		if rates,ok:=data["quotes"].(map[string]interface{});ok{
+			u2c_rate := rates["USDCNY"].(float64)
+			if model == "USD"{
+				rate = 1.0 / u2c_rate
+			}else{
+				base_rate := rates["USD"+model].(float64)
+				rate = base_rate / u2c_rate
+			}
+		}else{
+			retry=true
+		}
+	}else{
+		retry=true
+	}
+
+	if retry{
 		data, err = request(API_URL_2, API_KEY_2, model)
 		if err != nil {
 			return ""
 		}
-		u2c_rate := data["rates"].(map[string]interface{})["CNY"].(float64)
-		base_rate := data["rates"].(map[string]interface{})[model].(float64)
-		rate = base_rate / u2c_rate
-	} else {
-		u2c_rate := data["quotes"].(map[string]interface{})["USDCNY"].(float64)
-		base_rate := data["quotes"].(map[string]interface{})["USD"+model].(float64)
-		rate = base_rate / u2c_rate
+		if rates,ok:=data["rates"].(map[string]interface{});ok{
+			u2c_rate := rates["CNY"].(float64)
+			base_rate := rates[model].(float64)
+			rate = base_rate / u2c_rate
+		}else{
+			return ""
+		}
 	}
+
 	result, _ := strconv.ParseFloat(amount, 64)
 	result /= rate
 
